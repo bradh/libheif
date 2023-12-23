437bdecdcbc7e8bef97b070ef691f87afe158b2c
Brad Hards <bradh@frogmouth.net>
2023-12-23 21:12:11 +1100

    uncompressed: convert tile-component interleave decoder to use BitReader

--- libheif/uncompressed_image.cc
+++ libheif/uncompressed_image.cc
@@ -697,30 +697,34 @@ public:
   {}
 
   Error decode(const std::vector<uint8_t>& uncompressed_data, std::shared_ptr<HeifPixelImage>& img) override {
-    const uint8_t* src = uncompressed_data.data();
-    uint64_t src_offset = 0;
+    BitReader srcBits(uncompressed_data.data(), (int)uncompressed_data.size());
 
     buildChannelList(img);
 
     for (ChannelListEntry &entry : channelList) {
       if (!entry.use_channel) {
         uint64_t bytes_per_component = entry.bytes_per_tile_row_src * entry.tile_height * m_uncC->get_number_of_tile_columns() * m_uncC->get_number_of_tile_rows();
-        src_offset += bytes_per_component;
+        srcBits.skip_bytes((int)bytes_per_component);
         continue;
       }
       for (uint32_t tile_row = 0; tile_row < m_uncC->get_number_of_tile_rows(); tile_row++) {
         for (uint32_t tile_column = 0; tile_column < m_uncC->get_number_of_tile_columns(); tile_column++) {
-          uint64_t dst_column_offset = tile_column * entry.bytes_per_tile_row_dest;
           for (uint32_t tile_y = 0; tile_y < entry.tile_height; tile_y++) {
             uint64_t dst_row_number = tile_row * entry.tile_height + tile_y;
             uint64_t dst_row_offset = dst_row_number * entry.dst_plane_stride;  
-            memcpy(entry.dst_plane + dst_row_offset + dst_column_offset, src + src_offset, entry.bytes_per_tile_row_dest);
-            src_offset += entry.bytes_per_tile_row_src;
+            for (uint32_t tile_x = 0; tile_x < entry.tile_width; tile_x++) {
+              uint64_t dst_col_number = tile_column * entry.tile_width + tile_x;
+              uint64_t dst_column_offset = dst_col_number * entry.bytes_per_component_sample;
+              int val = srcBits.get_bits(entry.bytes_per_component_sample * 8);
+              memcpy(entry.dst_plane + dst_row_offset + dst_column_offset, &val, entry.bytes_per_component_sample);
+            }
+            // There is a better way to do this...
+            srcBits.skip_bytes(entry.bytes_per_tile_row_src - entry.bytes_per_tile_row_dest);
           }
           if (m_uncC->get_tile_align_size() != 0) {
             // TODO: there is probably a cleaner way to do this...
-            while (src_offset % m_uncC->get_tile_align_size() != 0) {
-              src_offset += 1;
+            while (srcBits.get_current_byte_index() % m_uncC->get_tile_align_size() != 0) {
+              srcBits.skip_bytes(1);
             }
           }
         }
